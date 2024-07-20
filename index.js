90485210e5e650b85f7bbdd96b2dee48476a6a74
sparky0520 <132695189+sparky0520@users.noreply.github.com>
2024-07-20 14:58:20 +0530

    Payments Implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import DiamSdk from 'diamante-sdk-js'
 import createChildAccount from './createChildAccount.js'
 import getAccountDetails from './getAccountDetails.js'
+import makePayment from './makePayment.js'
 
 const pair = DiamSdk.Keypair.random()
 
@@ -9,11 +10,18 @@ console.log("Private Key: ", pair.secret())
 
 console.log("\n\n\n---------------Funding Parent Account with Friendly Bot with 10,000 Diams------------")
 console.log("---------------Creating Child Account with Parent Account with 5 Diams------------\n\n")
-const childAccount = await createChildAccount(pair)   // returns childAccount keypairs
+const child1 = await createChildAccount(pair)   // returns child1 keypairs
 
-console.log("\n\n\n---------------Getting Account Details------------\n\n")
-const account = await getAccountDetails(childAccount)
-if (account) {
-    console.log("\n\n\n\n\n\nFull Account Details:", account)
-}
+// console.log("\n\n\n---------------Getting Account Details------------\n\n")
+// const account = await getAccountDetails(childAccount)
+
+// if (account) {
+//     console.log("\n\n\n\n\n\nFull Account Details:", account)
+// }
+
+// Making another child account
+const child2 = await createChildAccount(pair)
+
+// Making transaction from child1 to child2 (1 Diam)
+makePayment(child1,child2.publicKey())
 
